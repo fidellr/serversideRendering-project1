b17acead91c0f440a44ad38cc09364a5f5ef773a
Fidel Ramadhan <fidelramadhan@gmail.com>
2017-10-16 03:13:49 +0700

    Update helpers.js

--- utils/helpers.js
+++ utils/helpers.js
@@ -3,6 +3,6 @@ export const getTokenFromCookie = (req) => {
   if (!req.headers.cookie) return null
   const token = req.headers.cookie.split(';').find(c => c.trim().startsWith('token='))
   if (!token) return null
-  const ID = token.split('=')[1]
-  return ID
+  const TOKEN = token.split('=')[1]
+  return TOKEN
 }
